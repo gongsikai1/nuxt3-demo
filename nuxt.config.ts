bb42331aa2f737635dbbe7cafc95f3f608773926
gongsikai <gongsikai@189.cn>
2025-04-18 09:44:17 +0800

    mod: change port

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiHost: 'api.demo.com',
-      apiPort: 3003
+      apiPort: 3001
     }
   },
   devtools: { enabled: true },
